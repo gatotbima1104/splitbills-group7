4fb87fd6839004bd8d4fc606f640f068c77b9704
gatotbima1104 <gatotbima2002@gmail.com>
2025-03-10 15:14:00 +0700

    Initial Commit

---
+++ splitbills.group7/splitbills_group7App.swift
@@ -0,0 +1,17 @@
+//
+//  splitbills_group7App.swift
+//  splitbills.group7
+//
+//  Created by Muhamad Gatot Supiadin on 10/03/25.
+//
+
+import SwiftUI
+
+@main
+struct splitbills_group7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
